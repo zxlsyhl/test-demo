910e65c4b1a883d0458adda79bc1eb9c3e17568c
= <=>
2019-12-25 11:27:56 +0800

    test

--- src/main/java/zxl/junit/bittest/BitTestOne.java
+++ src/main/java/zxl/junit/bittest/BitTestOne.java
@@ -86,4 +86,29 @@ public class BitTestOne {
     public void test7(){
         System.out.println(0x2168);
     }
+
+    @Test
+    public void test8(){
+        String a = "张";
+        char b = a.charAt(0);
+        byte c = a.getBytes()[0];
+        System.out.println(b);
+        System.out.println(c);
+        char[] d = a.toCharArray();
+        System.out.println("print d:");
+        for (char dd:d
+             ) {
+            System.out.println(dd);
+        }
+        byte[] e = a.getBytes();
+        System.out.println("print e:");
+        for (byte ee: e){
+            int eee = ee;
+            System.out.println(ee + ";;;;"+ Integer.toBinaryString(eee));
+        }
+        System.out.println("二进制字符串转十进制：");
+        int f = 0b11111111111111111111111111100101;
+        System.out.println(f);
+
+    }
 }
