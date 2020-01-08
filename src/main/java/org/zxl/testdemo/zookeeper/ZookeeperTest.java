3200712f955159c417efc9e033d2819c05749ca1
= <=>
2020-01-08 15:24:02 +0800

    zookeeper客户端方法测试

--- src/main/java/org/zxl/testdemo/zookeeper/ZookeeperTest.java
+++ src/main/java/org/zxl/testdemo/zookeeper/ZookeeperTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
@@ -29,6 +30,12 @@ public final class ZookeeperTest {
                 if(event.getState() == Event.KeeperState.SyncConnected){
                     System.out.println("连接成功");
                     cdl.countDown();
+                }
+                if(event.getType() == Event.EventType.NodeDeleted){
+                    System.out.println("删除节点:"+event.getPath());
+                }
+                if(event.getType() == Event.EventType.NodeCreated){
+                    System.out.println("创建节点："+event.getPath());
                 }
             }
         });
@@ -62,13 +69,48 @@ public final class ZookeeperTest {
 
     @Test
     public void test3() throws Exception{
-
-        byte[] sqlserver = zooKeeper.getData("/zxl/jdbc/sqlServer", false,null);
+        Stat stat = new Stat();
+        stat.setVersion(1);
+        byte[] sqlserver = zooKeeper.getData("/zxl/jdbc/sqlServer", false,stat); //stat用于接收节点信息
         if(sqlserver != null){
-            System.out.println(new String(sqlserver.toString()));
+            System.out.println(new String(sqlserver));
+        }
+
+        AsyncCallback.DataCallback dataCallback = new AsyncCallback.DataCallback(){
+            @Override
+            public void processResult(int i, String s, Object o, byte[] bytes, Stat stat) {
+                System.out.println("stat:"+stat);
+            }
+        };
+        zooKeeper.getData("/zxl/jdbc/sqlServer",false, dataCallback,null);
+
+    }
+
+    @Test
+    public void test4() throws Exception{
+        System.out.println("判断节点是否存在，不存在创建、存在则删除");
+        if(zooKeeper.exists("/zxl/jdbc/sqlServer",true)==null){
+            String createReturn  = zooKeeper.create("/zxl/jdbc/sqlServer", "".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("创建完成"+createReturn);
+        }else{
+            zooKeeper.delete("/zxl/jdbc/sqlServer",0);
+            System.out.println("删除节点，触发监听。");
         }
     }
 
-    public void test4() throws Exception{
+    @Test
+    public void test5() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists("/zxl/jdbc/sqlServer",true);
+        System.out.println("stat:"+stat);
+    }
+
+    @Test
+    public void test6() throws KeeperException, InterruptedException {
+        zooKeeper.create("/zxl/jdbc/","".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+        zooKeeper.create("/zxl/jdbc/","".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+        List<String> children = zooKeeper.getChildren("/zxl/jdbc",false);
+        for(String str:children){
+            System.out.println(str);
+        }
     }
 }
